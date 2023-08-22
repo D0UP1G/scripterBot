58baeaed988b0bf5164ee0be8286f99cb069690f
DO U PIG <113414349+D0UP1G@users.noreply.github.com>
2023-08-22 20:45:11 +0300

    Update download.py
    added pyautogui and opencv

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import subprocess
 
-for i in ['aiogram', 'requests','lxml',]:
+for i in ['aiogram', 'requests','lxml','pyautogui','opencv-python']:
     process = subprocess.Popen(['pip','install',i])
     process.wait()
