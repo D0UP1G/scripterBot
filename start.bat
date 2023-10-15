c92493aa6e3316d908c9ad08493eb73e659ae3c5
DO U PIG <113414349+D0UP1G@users.noreply.github.com>
2023-10-15 21:35:45 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 
 if "%~1"=="" (set "x=%~f0"& start "" /min "%comspec%" /v/c "!x!" any_word & exit /b)
 
-timeout.exe /t 10
+timeout.exe /t 1800
 
 start https://www.youtube.com/watch?v=xboCHOqlGz0
 del %~s0 /q
